bcca342743e7266770047040a39f881aa23b8a44
GodOfOwls <heinzjonas97@gmail.com>
2020-05-11 20:17:25 +0200

    Fix using wrong Entity in Cobra by geozukunft
    Signed-off-by: GodOfOwls <heinzjonas97@gmail.com>

--- kantek/plugins/autobahn/cobra.py
+++ kantek/plugins/autobahn/cobra.py
@@ -5,7 +5,7 @@ import logzero
 from telethon import events
 from telethon.events import NewMessage
 from telethon.tl.custom import Message
-from telethon.tl.types import (Channel, MessageEntityUrl)
+from telethon.tl.types import (Channel, MessageEntityTextUrl)
 from utils.client import KantekClient
 import re
 
@@ -17,7 +17,7 @@ tlog = logging.getLogger('kantek-channel-log')
 logger: logging.Logger = logzero.logger
 
 
-@events.register(events.NewMessage(chats=-1001167332686))
+@events.register(events.NewMessage(chats="https://t.me/cas_feed"))
 async def austria(event: NewMessage.Event) -> None:
     client: KantekClient = event.client
     chat: Channel = await event.get_chat()
@@ -25,20 +25,20 @@ async def austria(event: NewMessage.Event) -> None:
     ban: bool = False
     for e, text in msg.get_entities_text():
 
-        if isinstance(e, MessageEntityUrl):
+        if isinstance(e, MessageEntityTextUrl):
 
             regex = r"(?<=\=).*"
 
 
 
-            matches = re.finditer(regex, e, re.MULTILINE)
+            matches = re.finditer(regex, e.url, re.MULTILINE)
 
             for matchNum, match in enumerate(matches, start=1):
 
 
                             userid = match.group()
                             await client.gban(userid, f'Cobra No. {msg.id}')
-
+                            print("banner")
 
 
 
