7936d1f646b9b65b921da50959879cf1e378568b
Viktor geozukunft <geozukunft@gmail.com>
2020-05-12 02:31:39 +0200

    Fixed time counter on readall and added function to only clear mentions from gban group

--- kantek/plugins/private/readall.py
+++ kantek/plugins/private/readall.py
@@ -6,11 +6,13 @@ from telethon import events
 from telethon.events import NewMessage
 from telethon.tl.custom import Dialog
 from telethon.tl.types import Channel, Chat, User
+from asyncio import sleep
 
 from config import cmd_prefix
 from utils import helpers
 from utils.client import KantekClient
 from utils.mdtex import Bold, Italic, KeyValueItem, MDTeXDocument, Section, SubSection
+from config import gban_group
 
 __version__ = '0.0.1'
 
@@ -30,5 +32,18 @@ async def readall(event: NewMessage.Event) -> None:
 
     stop_time = time.time() - start_time
 
-    await client.respond(event, 'Took' + stop_time, reply=False)
+    await client.respond(event, 'Took' + str(stop_time), reply=False)
     await waiting_message.delete()
+
+
+@events.register(events.NewMessage(outgoing=True, pattern=f'{cmd_prefix}rmmention'))
+async def rmmention(event: NewMessage.Event) -> None:
+    client: KantekClient = event.client
+    waiting_message = await client.respond(event, 'Clearing mentions in your bangroup.')
+    for x in range(3):
+        await client.send_read_acknowledge(gban_group, clear_mentions=True)
+        await sleep(500)
+
+    await client.respond(event, 'Done', reply=False)
+    await waiting_message.delete()
+
