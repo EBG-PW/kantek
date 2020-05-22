fde490989a1f763bcc375d7e1fb461b62f5aea4e
Viktor geozukunft <geozukunft@gmail.com>
2020-05-22 21:49:43 +0200

    Added adminlist function which gets the list of all chats where user is admin or creator

---
+++ kantek/plugins/private/admincheck.py
@@ -0,0 +1,33 @@
+import logging
+from typing import Dict, Union
+import asyncio
+import logzero
+from telethon import events
+from telethon.errors import UserIdInvalidError
+from telethon.events import ChatAction, NewMessage, UserUpdate
+from telethon.tl.types import Channel, ChannelParticipantsAdmins, Chat
+from telethon.tl.custom import Message
+from utils.client import KantekClient
+from config import cmd_prefix
+
+
+@events.register(events.NewMessage(outgoing=True, pattern=f'{cmd_prefix}adminlist'))
+async def adminlist(event: NewMessage.Event) -> None:
+    client: KantekClient = event.client
+    waiting_message = await client.respond(event, 'Checking Admin status')
+    admin_in_groups = 0
+    admin_groups = []
+    message = ''
+
+    async for dialog in client.iter_dialogs():
+        entity = dialog.entity
+
+        if isinstance(entity, Chat):
+            if entity.creator or entity.admin_rights:
+                admin_in_groups += 1
+                admin_groups.append(entity.id)
+
+
+    for id in admin_groups:
+        chat: Chat = await client.get_entity(id)
+        message += chat.title + "\n"
