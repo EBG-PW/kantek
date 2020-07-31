fccb0288c861428c613b151633005f299e1718da
GodOfOwls <heinzjonas97@gmail.com>
2020-07-31 02:01:25 +0200

    fix user_info on local banned but not spamwatch banned users

--- kantek/plugins/private/user.py
+++ kantek/plugins/private/user.py
@@ -132,12 +132,12 @@ async def _collect_user_info(client, user, db, **kwargs) -> Union[str, Section, 
     ban_reason = db.banlist.get_user(user.id)
     if ban_reason:
         ban_reason = ban_reason['reason']
-        if client.sw and client.sw.permission.value <= Permission.User.value:
-            sw_ban = client.sw.get_ban(int(user.id))
-            if sw_ban:
-                ban_message = sw_ban.message
-                if ban_message and not full_ban_msg:
-                    ban_message = f'{ban_message[:128]}{"[...]" if len(ban_message) > 128 else ""}'
+    if client.sw and client.sw.permission.value <= Permission.User.value:
+        sw_ban = client.sw.get_ban(int(user.id))
+        if sw_ban:
+            ban_message = sw_ban.message
+            if ban_message and not full_ban_msg:
+                ban_message = f'{ban_message[:128]}{"[...]" if len(ban_message) > 128 else ""}'
 
     if id_only:
         return KeyValueItem(title, Code(user.id))
