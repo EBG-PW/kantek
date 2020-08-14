396d0044aaf08698287ac88ffd9961ff8ab61ade
GodOfOwls <heinzjonas97@gmail.com>
2020-08-14 16:10:56 +0200

    add .system command

---
+++ kantek/plugins/private/system.py
@@ -0,0 +1,57 @@
+import logging
+import subprocess
+import time
+import json
+
+from telethon.tl.custom import Dialog
+from telethon.tl.types import Channel, Chat, User
+
+from utils import helpers
+from utils.client import Client
+from utils.mdtex import *
+from utils.pluginmgr import k, Command
+
+tlog = logging.getLogger('kantek-channel-log')
+
+
+@k.command('system')
+async def system(client: Client, event: Command) -> MDTeXDocument:
+    """Collect stats about the system where kantek is running
+
+    Examples:
+        {cmd}
+    """
+    waiting_message = await client.respond(event, 'Collecting stats. This might take a while.')
+    start_time = time.time()
+
+    try:
+        fetch = subprocess.run(['owlfetch', '--json'], stdout=subprocess.PIPE)
+
+    except FileNotFoundError:
+        response = MDTeXDocument(Section(Bold('Something went wrong')))
+        return response
+
+    system_info = json.loads(fetch.stdout)
+    stop_time = time.time() - start_time
+
+    response = MDTeXDocument(
+
+        Section(Bold(f'Stats for Kanteksystem'),
+
+                SubSection('Hardware',
+                           KeyValueItem('Host', system_info['Host']),
+                           KeyValueItem('CPU', system_info['CPU']),
+                           KeyValueItem('GPU', system_info['GPU']),
+                           KeyValueItem('Memory', system_info['Memory']),
+                           KeyValueItem('Uptime', system_info['Uptime'])),
+                SubSection('Software',
+                           KeyValueItem('OS', system_info['OS']),
+                           KeyValueItem('Kernel', system_info['Kernel']),
+                           KeyValueItem('Terminal', system_info['Terminal']),
+                           KeyValueItem('Shell', system_info['Shell']),
+                           KeyValueItem('Packages', system_info['Packages'])),
+
+                Italic(f'Took {stop_time:.02f}s')))
+
+    await waiting_message.delete()
+    return response
