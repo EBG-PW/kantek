6c8ffd615d22ca48c35bda4aed6a00011ff4f8e5
GodOfOwls <heinzjonas97@gmail.com>
2020-09-01 20:52:05 +0200

    add gkick command to activly kick (and permanently ban) an user from all 'enl-only' chats

---
+++ kantek/plugins/private/gkick.py
@@ -0,0 +1,69 @@
+""""Plugin to get the fbans of the most important feds of a User."""
+import asyncio
+import logging
+from typing import Union, Dict, List, Optional
+
+from telethon.errors import MessageIdInvalidError
+from telethon.tl.custom import Forward, Message
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import MessageEntityMention, MessageEntityMentionName, User, Channel, UserFull, InputUser
+
+from utils import helpers, constants
+from utils.client import Client
+from utils.mdtex import *
+from utils.pluginmgr import k
+from utils.tags import Tags
+
+__version__ = '0.1.2'
+
+tlog = logging.getLogger('kantek-channel-log')
+
+
+@k.command('globalkick', 'gkick')
+async def gkick(msg: Message, tags: Tags, client: Client, db, args: List):
+    waiting_message = await client.send_message(msg.chat, 'Starting cleanup. This might take a while.')
+
+    entities: List = []
+    for uid in args:
+        if isinstance(uid, int):
+            entities.append(uid)
+
+    userid: int = entities[0]
+
+    # noinspection PyTypeChecker
+    offender: InputUser = await client.get_input_entity(userid)
+    offender_full: UserFull = await client(GetFullUserRequest(offender))
+    common = offender_full.common_chats_count
+    modulus = (common // 2) or 1
+    user_counter = 0
+
+    async for dialog in client.iter_dialogs():
+        entity = dialog.entity
+
+        if isinstance(entity, Channel):
+
+            chat_tags = await tags.from_id(db, int('-100' + str(entity.id)), private=False)
+            print(chat_tags)
+            enl = chat_tags.get('enl-only', False)
+
+            if not enl:
+                continue
+
+            if not entity.admin_rights or entity.creator:
+                continue
+
+            if waiting_message is not None and user_counter % modulus == 0:
+                user_counter += 1
+                progress = Section('Cleanup',
+                                   KeyValueItem(Bold('Progress'),
+                                                f'{user_counter}/{common}'))
+                try:
+                    await waiting_message.edit(str(progress))
+                except MessageIdInvalidError:
+                    waiting_message = None
+            user_counter += 1
+
+            try:
+                await client.ban(dialog, userid)
+            except Exception as e:
+                tlog.critical(e)
