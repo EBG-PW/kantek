6aa1dd1463b67d1e4a7542cd565b6c62e75e2173
GodOfOwls <heinzjonas97@gmail.com>
2020-09-02 21:11:54 +0200

    dont automaticlly report to spamwatch

--- kantek/plugins/private/rappelport.py
+++ kantek/plugins/private/rappelport.py
@@ -8,20 +8,29 @@ from telethon import TelegramClient
 
 
 @k.command('report', delete=True)
-async def report(client: Client, chat: Channel, msg: Message, args, event):
+async def report(client: Client, chat: Channel, msg: Message, args, kwargs, event):
     config = Config()
+
+    sw_report = kwargs.get('sw', False)
+
     if msg.is_reply:
         reply: Message = await msg.get_reply_message()
-        client2: TelegramClient = TelegramClient('reporter', api_id=config.api_id, api_hash=config.api_hash)
-        await client2.connect()
-        if not await client2.is_user_authorized():
-            await client2.start(config.phone_reporter)
+
         result = await reply.forward_to(-1001187874753)
-        async with client2:
-            await client2.get_dialogs()
 
-            reported = await client2.forward_messages(messages=result.id, from_peer=PeerChannel(channel_id=1187874753), entity=PeerChannel(channel_id=1275988180))
+        if sw_report:
 
-            print(reported)
+            client2: TelegramClient = TelegramClient('reporter', api_id=config.api_id, api_hash=config.api_hash)
+            await client2.connect()
+            if not await client2.is_user_authorized():
+                await client2.start(config.phone_reporter)
+
+            async with client2:
+                await client2.get_dialogs()
+
+                reported = await client2.forward_messages(messages=result.id, from_peer=PeerChannel(channel_id=1187874753),
+                                                          entity=PeerChannel(channel_id=1251251585))
+
+                print(reported)
     else:
         return
