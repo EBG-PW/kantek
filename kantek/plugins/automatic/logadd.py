1487095ab0aabb71bf832f6f147af7660fe62656
GodOfOwls <heinzjonas97@gmail.com>
2020-10-02 23:30:11 +0200

    finish logadder

--- kantek/plugins/automatic/logadd.py
+++ kantek/plugins/automatic/logadd.py
@@ -1,9 +1,12 @@
 from html import escape
 
 import re
+
+
+from kantex.md import *
 from telethon import events
 from telethon.errors import MessageIdInvalidError
-from telethon.events import NewMessage, chataction
+from telethon.events import NewMessage, ChatAction
 from telethon.tl.custom import Message
 from telethon.tl.types import Channel, User
 from telethon.utils import get_display_name
@@ -24,21 +27,29 @@ from utils.tags import Tags
 
 
 @k.event(events.ChatAction, name='logadder')
-async def admin_reports(e: chataction.Event) -> None:
-    if (not e.user_added) and (not e.created):
+async def logadder(e: ChatAction) -> None:
+    client: Client = e.client
+    if not e.user_added:
         return
-    me = await e.client.get_peer_id('me')
+    me: User = await client.get_entity('me')
     if e.added_by == me:
         return
     a = e.action_message
     if not e.created:
-        if me not in a.action.users:
+        if me.id not in a.action.users:
             return
-    chat = await #Hier den chat getten
-#    print(a.stringify())
+    chat: Channel = await e.get_chat()
+
     adder: User = await e.client.get_entity(a.from_id)
     adder_name = adder.first_name
-    text = #Hier ein ordentlicher String mit chat adder und addername
+    chat_link = getattr(chat, 'username', None) or f'c/{chat.id}'
+    text = KanTeXDocument(Section('#ADDED',
+                                    KeyValueItem('Name', adder_name),
+                                    KeyValueItem(Link('Chat', f'https://t.me/{chat_link}/{e.action_message.id}'), chat.title),
+                                    KeyValueItem('Adder-ID', adder.id)
+                                  ))
 
-    await e.client.send_message(config.log_chat, text) #Hier den text in die log group schicken
+    await client.send_message(-1001187874753, str(text))
+
+
 
