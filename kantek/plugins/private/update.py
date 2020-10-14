23aaf5df8214fddd8b768043bd5fda21b96c85a3
jonas <heinzjonas97@gmail.com>
2020-10-15 00:25:34 +0200

    allow update by randoms

--- kantek/plugins/private/update.py
+++ kantek/plugins/private/update.py
@@ -10,7 +10,90 @@ from utils.tags import Tags
 
 @k.command('updateallplsbro', admins=True)
 async def update2(client: Client, event: Command, tags: Tags) -> None:
-    await update(client, event, tags)
+    """Run git pull and exit.
+
+    This command assumes the bot is running under a process manager that automatically restarts it.
+
+    Tags:
+        update: Set to `silent` to silence any messages
+
+    Examples:
+        {cmd}
+    """
+    silent = tags.get('update', True)
+    old_commit = helpers.get_commit()
+    # region git pull
+    if not silent:
+        progess_message = await client.respond(event, KanTeXDocument(
+            Section('Updating',
+                    f'Running {Code("git pull")}')))
+    else:
+        await event.delete()
+
+    proc = subprocess.call(['git', 'pull', '-q'])
+    if proc != 0:
+        msg = KanTeXDocument(
+            Section('Error',
+                    f'{Code("git")} returned non-zero exit code.',
+                    'Please update manually'))
+        if not silent:
+            await progess_message.edit(str(msg))
+        else:
+            await client.respond(event, msg)
+        return
+    # endregion
+
+    new_commit = helpers.get_commit()
+    if old_commit == new_commit:
+        await client.respond(
+            event, KanTeXDocument(
+                Section('No Update Available',
+                        Italic('Doing nothing'))))
+        return
+
+    # region pip install
+    proc = subprocess.call(['pip', 'install', '-r', 'requirements.txt'])
+    if proc != 0:
+        msg = KanTeXDocument(
+            Section('Error',
+                    f'{Code("pip")} returned non-zero exit code.',
+                    'Please update manually'))
+        if not silent:
+            await progess_message.edit(str(msg))
+        else:
+            await client.respond(event, msg)
+        return
+    # endregion
+
+    # region migrant
+    '''
+    if not silent:
+        await progess_message.edit(str(KanTeXDocument(
+            Section('Updating',
+                    f'Running {Code("migrant apply --all")}'))))
+    proc = subprocess.run(['migrant', 'apply', '--all'], stderr=subprocess.PIPE)
+    if proc.returncode != 0:
+        if b'MigrationComplete' not in proc.stderr:
+            msg = KanTeXDocument(
+                Section('Error',
+                        f'{Code("migrant")} returned non-zero exit code.',
+                        'Please update manually'))
+            if not silent:
+                await progess_message.edit(str(msg))
+            else:
+                await client.respond(event, msg)
+            return
+    # endregion
+    '''
+
+    if not silent:
+        await progess_message.delete()
+        await client.respond(
+            event, KanTeXDocument(
+                Section('Updated Kantek',
+                        KeyValueItem('New commit', Link(new_commit, helpers.link_commit(new_commit))),
+                        Italic('Restarting bot'))))
+    await client.disconnect()
 
 
 @k.command('update')
@@ -25,7 +108,7 @@ async def update(client: Client, event: Command, tags: Tags) -> None:
     Examples:
         {cmd}
     """
-    silent = tags.get('update', True)
+    silent = tags.get('update', False)
     old_commit = helpers.get_commit()
     # region git pull
     if not silent:
