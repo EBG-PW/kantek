03cd301f373462db1990a95b15baaaca7de6c37b
jonas <jonas@ebg.pw>
2020-11-30 04:37:33 +0100

    add saving of mentioned users

--- kantek/plugins/private/KuKGebirgstruppe.py
+++ kantek/plugins/private/KuKGebirgstruppe.py
@@ -1,11 +1,15 @@
 import logging
 
 from telethon import events
+from telethon.errors import ChannelPrivateError, FloodWaitError
 from telethon.events import NewMessage
 from telethon.tl.custom import Message
+from telethon.tl.types import MessageEntityMention, Channel
 
 from database.database import Database
+from utils import constants
 from utils.client import Client
+from utils.constants import GET_ENTITY_ERRORS
 from utils.pluginmgr import k
 
 tlog = logging.getLogger('kantek-channel-log')
@@ -14,6 +18,8 @@ tlog = logging.getLogger('kantek-channel-log')
 @k.event(events.MessageEdited(outgoing=False))
 @k.event(events.NewMessage(outgoing=True, incoming=True), name='Kaiserkönigliche Gebirgstruppe')
 async def KuK(event: NewMessage.Event) -> None:
+    if event.is_private:
+        return
     client: Client = event.client
     db: Database = client.db
     self_id: int = client.self_id
@@ -23,3 +29,24 @@ async def KuK(event: NewMessage.Event) -> None:
     uid: int = msg.sender_id
     hash: str = msg.sender.access_hash
     result = await db.hashlist.add_user(uid, self_id, name, hash)
+
+    entities = msg.get_entities_text()
+    for entity, text in entities:  # pylint: disable = R1702
+        if isinstance(entity, MessageEntityMention):
+            _entity = text
+            if _entity:
+                try:
+                    try:
+                        full_entity = await client.get_cached_entity(_entity)
+                    except (FloodWaitError, *GET_ENTITY_ERRORS):
+                        full_entity = None
+                    if full_entity:
+                        if isinstance(full_entity, Channel):
+                            return
+                        mention_id = full_entity.id
+                        mention_name = full_entity.first_name
+                        mention_hash = full_entity.access_hash
+                        await db.hashlist.add_user(mention_id, self_id, mention_name, mention_hash)
+
+                except (*constants.GET_ENTITY_ERRORS, ChannelPrivateError):
+                    pass
