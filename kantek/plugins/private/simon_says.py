6d711f2510e1446a9fdcc128f57546a68de611a8
jonas <jonas@ebg.pw>
2020-12-25 02:36:17 +0100

    use regex to remove neccesary -sudo to make triggering sudo a choice

--- kantek/plugins/private/simon_says.py
+++ kantek/plugins/private/simon_says.py
@@ -8,16 +8,15 @@ from database.database import Database
 from utils.client import Client
 from utils.pluginmgr import k, Command
 from utils.tags import Tags
+import re
 
 
 @k.command('say', 's', sudos=True)
 async def simon_says(client: Client, db: Database, tags: Tags, chat: Channel, msg: Message,
-               args: List, kwargs: Dict, event: Command) -> Optional[KanTeXDocument]:
-
-
+                     args: List, kwargs: Dict, event: Command) -> Optional[KanTeXDocument]:
     text: str = msg.raw_text
     new_text = text.split(' ', 1)
-    to_send: str = new_text[1]
-    to_send: str = to_send.strip('\\-sudo')
+    to_send1: str = new_text[1]
+    to_send2: str = re.sub('-sudo', '', to_send1)
 
-    await client.send_message(chat, f'​{to_send}')
+    await client.send_message(chat, f'​{to_send2}')
