f7afe80fdcfa010ff35f8c02086e4df101fab1b2
jonas <jonas@ebg.pw>
2021-02-19 23:13:13 +0100

    log spam adding bans

--- kantek/plugins/autobahn/automatic/kommando.py
+++ kantek/plugins/autobahn/automatic/kommando.py
@@ -1,11 +1,12 @@
 import logging
 from typing import Union, Dict
 
+from kantex.md import *
 import logzero
 from telethon import events
 from telethon.events import ChatAction, NewMessage
 from telethon.tl.types import (MessageActionChatJoinedByLink,
-                               MessageActionChatAddUser, MessageService)
+                               MessageActionChatAddUser, MessageService, User, Channel)
 
 from database.database import Database
 from utils.client import Client
@@ -46,6 +47,7 @@ async def ksk(event: Union[ChatAction.Event, NewMessage.Event]) -> None:  # pyli
             return
     client: Client = event.client
     db: Database = client.db
+    chat: Channel = await event.get_chat()
     tags = await Tags.from_event(event)
     polizei_tag = tags.get('polizei')
     ksk_tag = tags.get('ksk')
@@ -58,6 +60,12 @@ async def ksk(event: Union[ChatAction.Event, NewMessage.Event]) -> None:  # pyli
         if isinstance(msg.action, MessageActionChatAddUser):
             action: MessageActionChatAddUser = msg.action
             if msg.sender_id not in action.users:
+                try:
+                    if (await client.get_cached_entity(action.users[0])).bot:
+
+                        return
+                except:
+                    pass
                 uid: int = msg.sender_id
 
                 cnt = spammers.get(uid, 0)
@@ -65,7 +73,21 @@ async def ksk(event: Union[ChatAction.Event, NewMessage.Event]) -> None:  # pyli
 
                 current_count = cnt + 1
                 if current_count > 20:
-                    if len(spammers) > 2000:
+                    if len(spammers) > 200:
                         spammers = {}
                     spammers[msg.sender_id] = 0
                     await client.gban(uid, f'spam adding {current_count}+ members')
+                    adder: User = await client.get_entity(msg.sender_id)
+                    adder_name = adder.first_name
+                    chat_link = getattr(chat, 'username', None) or f'c/{chat.id}'
+                    text = KanTeXDocument(Section('#ADDED',
+                                                  KeyValueItem('Name', adder_name),
+                                                  KeyValueItem(
+                                                      Link('Chat', f'https://t.me/{chat_link}/{event.action_message.id}'),
+                                                      chat.title),
+                                                  KeyValueItem('Adder-ID', adder.id)
+                                                  ))
+
+                    await client.send_message(-1001418023497, str(text))
+
+
