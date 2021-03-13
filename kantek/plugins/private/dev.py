9004da24ccdb850fd33f59903f2a2dfd686b17f3
jonas <heinzjonas97@gmail.com>
2021-03-13 14:15:05 +0100

    fix redudnandt functions thx @TheKneeSocks
    Signed-off-by: jonas <heinzjonas97@gmail.com>

--- kantek/plugins/private/dev.py
+++ kantek/plugins/private/dev.py
@@ -16,35 +16,35 @@ async def term(event):
     Examples:
         {cmd} command
     """
-    async def shell(event):
-        if event.fwd_from:
+
+    if event.fwd_from:
+        return
+    cmd = event.text.split(" ", 1)
+    if len(cmd) == 1:
+        return
+    else:
+        cmd = cmd[1]
+    async_process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await async_process.communicate()
+    msg = f"**Command:**\n`{cmd}`\n"
+    if stderr.decode():
+        msg += f"**Stderr:**\n`{stderr.decode()}`"
+    if stdout.decode():
+        msg += f"**Stdout:**\n`{stdout.decode()}`"
+    if len(msg) > 4096:
+        with io.BytesIO(msg) as file:
+            file.name = "shell.txt"
+            await Client.send_file(
+                event.chat_id,
+                file,
+                force_document=True,
+                caption=cmd,
+                reply_to=event.message.id,
+            )
             return
-        cmd = event.text.split(" ", 1)
-        if len(cmd) == 1:
-            return
-        else:
-            cmd = cmd[1]
-        async_process = await asyncio.create_subprocess_shell(
-            cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-        )
-        stdout, stderr = await async_process.communicate()
-        msg = f"**Command:**\n`{cmd}`\n"
-        if stderr.decode():
-            msg += f"**Stderr:**\n`{stderr.decode()}`"
-        if stdout.decode():
-            msg += f"**Stdout:**\n`{stdout.decode()}`"
-        if len(msg) > 4096:
-            with io.BytesIO(msg) as file:
-                file.name = "shell.txt"
-                await Client.send_file(
-                    event.chat_id,
-                    file,
-                    force_document=True,
-                    caption=cmd,
-                    reply_to=event.message.id,
-                )
-                return
-        await event.edit(msg)
+    await event.edit(msg)
 
 # Thanks to stackoverflow for existing https://stackoverflow.com/questions/3906232/python-get-the-print-output-in-an-exec-statement
 
